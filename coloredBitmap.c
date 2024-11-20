2c5c7c3d3d2fdf0fb6a336272d6746bce0fd48bd
lysa2004 <148379420+lysa2004@users.noreply.github.com>
2024-11-20 10:46:13 +0100

    Update coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,22 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
+   int value = (3 * (w * h * 8)) ;
 
+  printf("Taille en bits: %d\n", value);
+
+/*int tailleoctet = (3 * (w * h * 8)) /8 ; 
+ printf("Taille en bits: %d\n", tailleoctet);*/
+
+ if (strcmp(unit, "bt") == 0) {
+        return (float)value; 
+    } else if (strcmp(unit, "ko") == 0) {
+        return (float)value / 8 / 1024; 
+    } else if (strcmp(unit, "mo") == 0) {
+        return (float)value / 8 / 1024 / 1024;
+    } else if (strcmp(unit, "go") == 0) {
+        return (float)value / 8 / 1024 / 1024 / 1024;
+    }
    // YOUR CODE HERE - END
-   return 0;
+   return value;
 }
